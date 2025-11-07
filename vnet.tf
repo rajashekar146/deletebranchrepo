dc97f2e198c74237f4cf52c601bf40d38e026341
Rajashekar <rajashekarmanthena07@gmail.com>
2025-11-07 16:12:56 +0530

    vnet code

---
+++ vnet.tf
@@ -0,0 +1,7 @@
+resource "azurerm_virtual_network" "vnet" {
+    name = "vnet01"
+    address_space = ["172.16.0.0/16"]
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+  
+}
