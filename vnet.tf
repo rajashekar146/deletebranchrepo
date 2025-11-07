8368034d83e3e5937d67f965e17915ed2e36d919
Rajashekar <rajashekarmanthena07@gmail.com>
2025-11-07 16:16:50 +0530

    added subnet code

--- vnet.tf
+++ vnet.tf
@@ -5,3 +5,10 @@ resource "azurerm_virtual_network" "vnet" {
     location = azurerm_resource_group.rg.location
   
 }
+resource "azurerm_subnet" "subnet" {
+    address_prefixes = ["172.16.1.0/24"]
+    name = "subnet-01"
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+  
+}
